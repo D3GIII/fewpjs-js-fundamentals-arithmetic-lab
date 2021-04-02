3f616abdcdb9ab654685bfaafc2e5d686001dce9
D3GIII <deangrabowski3@gmail.com>
2021-04-02 16:25:40 -0400

    Assignment completed

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
-
 // Write your code here
+
+// Below is multiply 
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+
+// Below is random
+const random = Math.floor(Math.random() * 10) + 1;
+
+// Below is mod
+const num3 = 60;
+const num4 = 8
+const mod = num3 % num4;
+
+// Below is max
+const max = Math.max(1,20);
